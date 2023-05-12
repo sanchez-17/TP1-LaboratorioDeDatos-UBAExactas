c8fcfe8e21e42cd2df875dc98cc2c345b2578154
Gaston Sanchez <gasanchez@dc.uba.ar>
2023-05-11 23:15:14 -0300

    Mejora de funciones atomizar y columna rubro df1

--- main.py
+++ main.py
@@ -21,48 +21,39 @@ df3 = pd.read_csv(loccensales)
 df4 = pd.read_csv(dicdepto)
 df5 = pd.read_csv(dicclase)
 
-
+#%%
 #df1['productos'].value_counts()
 
 #a=df1.loc[df1['productos'].str.contains('HORTICULTURA',na=False)]
 
 
 def atomizarFila(df,col,fila,string):
-    fila2 = df.loc[fila,:]
-    aux=df.loc[fila,col].split(string)
-    for elem in aux:
-        fila_nueva= fila2
-        fila_nueva[col] = elem
-        df.loc[len(df)]= fila_nueva
-    #borramos la fila original
+    valores_atomicos = df.loc[fila, col].split(string)
+    for valor in valores_atomicos:
+        df.at[len(df), col] = valor
+    #borramos la fila original    
     df.drop([fila],inplace=True)
     df.reset_index(drop=True, inplace=True)
-    
-
 
 def atomizarColumna(df,col,string):
-    rango = len(df)
-    i = 0
-    while i < rango:
+    for i in range(len(df)):
         atomizarFila(df,col,0,string)
-        i += 1
 
 
 def quitar_parentesis(string):
+    #Quita todo lo que esta entre parentesis
     regex = r"\([^()]*\)"
     res = re.sub(regex, "", string)
     return res
 
-def quitar_punto(string):
-    return string.replace(".", "")
+def reemplazar(string,cadena,reemplazo):
+    return string.replace(cadena,reemplazo)
 
-        
-def quitar_dospuntos(string):
-   return string.replace(":", "")
+#%%
+#Sacamos los nan de columna productos
+df1.dropna(inplace=True,subset=["productos"])
 
-#df1.dropna(inplace=True,subset="productos")
-
-#-------Columna productos-------------
+"""-------Columna productos-------------"""
 col = "productos"
 #Spliteamos por comas
 atomizarColumna(df1,'productos',', ')
@@ -72,8 +63,8 @@ atomizarColumna(df1,'productos',' Y ')
 atomizarColumna(df1,'productos',';')
 #Spliteamos por -
 atomizarColumna(df1,'productos','-')
-
-df1['productos'] = df1['productos'].apply(quitar_punto)
+#%%
+#df1['productos'] = df1['productos'].apply(quitar_punto,args=('.'))
 
 def cambiar_todo_string(string,viejo,nuevo):
     if string.str.contains(viejo):
@@ -89,4 +80,110 @@ df_productos = df1.loc[:,['productos','razón social','establecimiento']]
 df_productos.dropna(inplace=True,subset="productos")
 atomizarColumna(df_productos,'productos', ',')
 df1.drop('productos',axis=1)
+"""
+Rubro separar por:
+    ?
+    +
+"""
+#%%
+"""COLUMNA RUBRO"""
+#Vemos los casos particulares
+sin_definir="INDEFINIDO"
+"""
+Veamos si para un producto."CAMPO INCULTO" aplica un valor en especifico para 
+la columna rubro
+"""
+#Filtramos nans con mascara para evitar mensaje de error:
+mask = ~df1.rubro.isna()
+df1_rubro_productos_sinNan = df1.loc[df1.productos.str.contains("INCULTO") & mask,["productos","rubro"]]
+#Vemos que INCULTO refiere al rubro AGRICULTURA, redefinimos los nan
+df1_producto_inculto_conNan = df1.loc[df1.rubro.isna(),["rubro","productos"]]
+df1.at[853, 'rubro'] = sin_definir
+df1.at[908, 'rubro'] = sin_definir
+df1.at[908, 'productos'] = sin_definir
+#luego aquellas las que tanto en productos y rubro tengo nan:
+mask = df1.rubro.isna() & df1.productos.isna()
+df1.loc[mask, ['rubro', 'productos']] = [sin_definir, sin_definir]
+#Para todos aquellos productos que tengan "INCULTO", definir en columna rubro AGRICULTURA
+filtro = df1.productos.str.contains("INCULTO")
+df1.loc[filtro,'rubro'] = "AGRICULTURA"
 
+    
+
+#Atomizando columna rubro
+#Veo la cantidad de nans: cant: 5
+#Pero ademas hay valores no  Nan pero definidos con "SIN DEFINIR" con str.contains
+#Pero tenenemos NaN, procedemos a quitarlos,luego veremos en particular:Solo son 5
+df1.dropna(inplace=True)
+#Genero serie booleana para encontrar lo que buscamos
+df1.rubro.str.contains("SIN DEFINIR").value_counts()
+#Hay 109 registros en rubro sin definir
+aux=df1.loc[df1.rubro.str.contains("SIN DEFINIR")]
+#Al parecer estos registros tienen en la columna categoria_desc el valor "Comercializadores", veamos si pasa con todos los reguistros
+bool1 = df1.categoria_desc.str.contains("Comercializadores")
+aux1 = df1.loc[bool1]
+#¿Es cierto que para todas las tuplas en donde categoria_desc="Comercializadores" pasa que rubro="SIN DEFINIR"?
+len(aux1) == len(aux)
+#Es verdad!
+
+#Veo los registros con el valor "OTROS " en columna rubro
+bool2=df1.rubro.str.contains("OTROS")
+aux3=df1.loc[bool2]
+#Hay 7 registros
+
+#Vemos la cantidad de valores unicos para poder atomizar aquellas tuplas que asi lo requieran
+c = df1.rubro.value_counts()
+"""
+Habra que separar por:
+    /
+    " Y "
+    ,
+    -
+    ;
+Ademas sacar los puntos
+y por ultimo sacar los espacios(el primer espacio y ultimo)
+
+"""
+col="rubro"
+atomizarColumna(df1,col,'/')
+#Verifico
+df1.rubro.str.contains("/").sum() #0
+
+atomizarColumna(df1,col,',')
+df1.rubro.str.contains(',').sum() #0
+
+atomizarColumna(df1,col,'-')
+df1.rubro.str.contains('-').sum() #0
+
+"""
+df1[col] = df1[col].apply(quitar_punto,args=('.'))
+#Es posible que tengamos problemas al separar por " Y "
+bool4 = df1.rubro.str.contains(" Y ")
+aux4 = df1.loc[bool4].rubro
+
+bool5 = df1.rubro.str.contains("EMPAQUE")
+aux5 = df1.loc[bool5,["rubro","productos"]]
+
+bool6 = df1.rubro.str.contains(" Y ")
+aux5 = df1.loc[bool4].rubro
+
+##Bueno, separamos por " Y "
+"""
+#%%
+df1.productos.str.contains("VID").count
+a=df1.productos=="UVAS"
+a.sum()
+b=df1.loc[df1.productos.str.contains("VINO"),["rubro","productos"]]
+#42 contiene UVAS
+c=df1.loc[df1.productos.str.contains("VID"),["rubro","productos"]]
+c=df1.loc[df1.productos.str.contains("VID")]
+
+#110 contiene VID
+d=df1.loc[df1.productos.str.contains("ACOPIO"),["rubro","productos"]]
+#%%
+df_provincia = df1[["provincia_id","provincia"]].drop_duplicates().reset_index(drop=True)
+df_pais = df1[["pais_id","pais"]].drop_duplicates().reset_index(drop=True)
+
+#%%
+df_provi = df[['D_NOMBRE', 'D_NUMERO', 'D_CUIL']]
+df_areas = df['D_AREAS'].str.split(',', expand=True).stack().reset_index(level=1, drop=True).reset_index(name='D_AREAS')

0e586f8e9f240d3d97b37baff127e6222041060e
Gaston Sanchez <gasanchez@dc.uba.ar>
2023-05-11 23:58:55 -0300

    funcion para contar nans dado un df

--- main.py
+++ main.py
@@ -89,10 +89,8 @@ Rubro separar por:
 """COLUMNA RUBRO"""
 #Vemos los casos particulares
 sin_definir="INDEFINIDO"
-"""
-Veamos si para un producto."CAMPO INCULTO" aplica un valor en especifico para 
-la columna rubro
-"""
+#Veamos si para un producto."CAMPO INCULTO" aplica un valor en especifico para 
+#la columna rubro
 #Filtramos nans con mascara para evitar mensaje de error:
 mask = ~df1.rubro.isna()
 df1_rubro_productos_sinNan = df1.loc[df1.productos.str.contains("INCULTO") & mask,["productos","rubro"]]
@@ -103,27 +101,22 @@ df1.at[908, 'rubro'] = sin_definir
 df1.at[908, 'productos'] = sin_definir
 #luego aquellas las que tanto en productos y rubro tengo nan:
 mask = df1.rubro.isna() & df1.productos.isna()
-df1.loc[mask, ['rubro', 'productos']] = [sin_definir, sin_definir]
-#Para todos aquellos productos que tengan "INCULTO", definir en columna rubro AGRICULTURA
-filtro = df1.productos.str.contains("INCULTO")
-df1.loc[filtro,'rubro'] = "AGRICULTURA"
+cols = ['rubro', 'productos']
+df1.loc[mask, cols] = [sin_definir for col in cols]
+
 
     
 
 #Atomizando columna rubro
-#Veo la cantidad de nans: cant: 5
-#Pero ademas hay valores no  Nan pero definidos con "SIN DEFINIR" con str.contains
-#Pero tenenemos NaN, procedemos a quitarlos,luego veremos en particular:Solo son 5
-df1.dropna(inplace=True)
 #Genero serie booleana para encontrar lo que buscamos
 df1.rubro.str.contains("SIN DEFINIR").value_counts()
 #Hay 109 registros en rubro sin definir
-aux=df1.loc[df1.rubro.str.contains("SIN DEFINIR")]
-#Al parecer estos registros tienen en la columna categoria_desc el valor "Comercializadores", veamos si pasa con todos los reguistros
-bool1 = df1.categoria_desc.str.contains("Comercializadores")
-aux1 = df1.loc[bool1]
+df1_rubro_sin_definir = df1.loc[df1.rubro.str.contains("SIN DEFINIR")]
+#Al parecer estos registros tienen en la columna categoria_desc el valor "Comercializadores", veamos si pasa con todos los registros
+filtro = df1.categoria_desc.str.contains("Comercializadores")
+df1_categoria_comercializadores = df1.loc[filtro]
 #¿Es cierto que para todas las tuplas en donde categoria_desc="Comercializadores" pasa que rubro="SIN DEFINIR"?
-len(aux1) == len(aux)
+len(df1_rubro_sin_definir) == len(df1_categoria_comercializadores)
 #Es verdad!
 
 #Veo los registros con el valor "OTROS " en columna rubro
@@ -155,7 +148,6 @@ df1.rubro.str.contains(',').sum() #0
 atomizarColumna(df1,col,'-')
 df1.rubro.str.contains('-').sum() #0
 
-"""
 df1[col] = df1[col].apply(quitar_punto,args=('.'))
 #Es posible que tengamos problemas al separar por " Y "
 bool4 = df1.rubro.str.contains(" Y ")
@@ -168,7 +160,11 @@ bool6 = df1.rubro.str.contains(" Y ")
 aux5 = df1.loc[bool4].rubro
 
 ##Bueno, separamos por " Y "
-"""
+
+
+#Para todos aquellos productos que tengan "INCULTO", definir en columna rubro AGRICULTURA
+filtro = df1.productos.str.contains("INCULTO")
+df1.loc[filtro,'rubro'] = "AGRICULTURA"
 #%%
 df1.productos.str.contains("VID").count
 a=df1.productos=="UVAS"
@@ -187,3 +183,15 @@ df_pais = df1[["pais_id","pais"]].drop_duplicates().reset_index(drop=True)
 #%%
 df_provi = df[['D_NOMBRE', 'D_NUMERO', 'D_CUIL']]
 df_areas = df['D_AREAS'].str.split(',', expand=True).stack().reset_index(level=1, drop=True).reset_index(name='D_AREAS')
+
+#%%
+def cuantos_nan(df):
+    nan_cant = df.isna().sum()
+    nan_porcentaje = (nan_cant / len(df)) * 100
+    res = pd.concat([nan_cant, nan_porcentaje], axis=1, keys=['Recuento de NaN', 'Porcentaje de NaN'])
+    res['Porcentaje de NaN'] = res['Porcentaje de NaN'].round(2).astype(str) + '%'
+    res.index = df.columns
+    print(res)
+#%%
+nans = df1.isna().sum()
+nans["%"]= df1.isna().sum() / len(df1) * 100

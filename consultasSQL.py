3a98d692188353dab2ac01c132552434164e6325
gaston sanchez <gasanchez@dc.uba.ar>
2023-06-26 19:30:27 -0300

    ej.3 SQL

--- consultasSQL.py
+++ consultasSQL.py
@@ -60,9 +60,9 @@ consigna = """Ejercicio 1:\n
 
 consultaSQL = """
                 SELECT DISTINCT
-                   count(*) AS cant_operadores,
-                   pr.nombre_provincia AS provincia
-                   FROM operador AS op
+                   count(*)                AS cant_operadores,
+                   pr.nombre_provincia     AS provincia
+                   FROM operador           AS op
                    INNER JOIN departamento AS depto
                    ON op.id_departamento = depto.id_departamento
                    RIGHT OUTER JOIN provincia AS pr
@@ -82,8 +82,8 @@ consigna = """Ejercicio 2:\n
 """    
 consultaSQL = """
                 SELECT DISTINCT
-                   count(*) AS cant_operadores, 
-                   depto.nombre AS departamento
+                   count(*)      AS cant_operadores, 
+                   depto.nombre  AS departamento
                    FROM operador AS op
                    RIGHT OUTER JOIN departamento AS depto
                    ON op.id_departamento = depto.id_departamento
@@ -97,21 +97,23 @@ imprimirEjercicio(consigna, [operador,departamento,provincia], consultaSQL)
 # Ejericicio 3
 
 consigna = """Ejercicio 3:\n
-                ¿Existen departamentos que no presentan Operadores Orgánicos
-                Certificados?\n
-                 ¿En caso de que sí, cuántos y cuáles son?
+                ¿Cuál es la actividad que más operadores tiene?
 """    
 consultaSQL = """
-                SELECT DISTINCT
-                   count(*) AS cant_operadores, 
-                   depto.nombre AS departamento
-                   FROM operador AS op
-                   RIGHT OUTER JOIN departamento AS depto
-                   ON op.id_departamento = depto.id_departamento
-                   RIGHT OUTER JOIN provincia AS prov
-                   ON depto.id_provincia = pr.id_provincia
-                   GROUP BY departamento
-                   HAVING cant_operadores = 0;
+                SELECT              
+                   MAX(cant_operadores)   AS maxima_cant_operadores,
+                   ANY_VALUE(descripcion) AS descripcion_clase,
+                   ANY_VALUE(id_clase)    AS id_clase
+                   FROM (
+                     SELECT
+                     count(op.id_operador) AS cant_operadores,
+                     clase.descripcion     AS descripcion,
+                     clase.id_clase        AS id_clase
+                     FROM operador         AS op
+                     RIGHT OUTER JOIN clase
+                     ON op.id_clase = clase.id_clase
+                     GROUP BY clase.id_clase, clase.descripcion
+                   )
               """
 
 imprimirEjercicio(consigna, [operador,departamento,provincia], consultaSQL)
